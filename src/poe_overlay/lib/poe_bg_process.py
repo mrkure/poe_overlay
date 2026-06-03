ea017e56540eacffe452e4ed87489dbcd3263472
mrkure <32226420+mrkure@users.noreply.github.com>
2026-06-03 20:52:27 +0200

    fix: fixed states array side (probably changed by diffrent version of numpy)

--- src/poe_overlay/lib/poe_bg_process.py
+++ src/poe_overlay/lib/poe_bg_process.py
@@ -13,14 +13,11 @@ def capture_screen(capture_ar, states_ar, params):
     frame_health_bar = params["frame_health_bar"]["geometry"]
     frame_mana_bar = params["frame_mana_bar"]["geometry"]
     capture_ar = np.frombuffer(capture_ar.get_obj(), dtype=np.uint8).reshape((window_geometry[3], window_geometry[2], 4))
-    states_ar = np.frombuffer(states_ar.get_obj(), dtype=np.uint)
+    states_ar = np.frombuffer(states_ar.get_obj(), dtype=np.uint32)
     monitor_grabber = mg.MonitorGrabber(window_geometry)
-
     while states_ar.item(0):
         capture_ar[:, :, :] = monitor_grabber.grab_geometry(window_geometry)
         health_value, _, _, _ = CaptureEvaluator.evaluate_health_bar(capture_ar, frame_health_bar)
         states_ar[5] = health_value
-
         mana_value, _, _, _ = CaptureEvaluator.evaluate_mana_bar(capture_ar, frame_mana_bar)
         states_ar[6] = mana_value
-        # print(mana_value)
